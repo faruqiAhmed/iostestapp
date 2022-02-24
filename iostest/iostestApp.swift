ba3994f4eae362539ebf953bb7731666f11e9413
Faruq <faruqdeveloper@gmail.com>
2022-02-24 11:48:19 +0600

    Initial Commit

---
+++ iostest/iostestApp.swift
@@ -0,0 +1,17 @@
+//
+//  iostestApp.swift
+//  iostest
+//
+//  Created by Md Omar Faruq on 2/24/22.
+//
+
+import SwiftUI
+
+@main
+struct iostestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
